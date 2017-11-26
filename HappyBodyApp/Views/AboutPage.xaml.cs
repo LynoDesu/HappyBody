ae98077ac68d29c084dd5dcbba532be5282ecd70
Adam Lynas <adam.lynas@ifsworld.com>
2017-11-26 20:24:05 +0530

    Added Xamarin.Forms projects and updated NuGet packages

---
+++ HappyBodyApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace HappyBodyApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
