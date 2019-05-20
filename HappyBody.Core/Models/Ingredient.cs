cdf582bb8e121e25db3b800baf61d87bcadabdcf
Adam Bandeira Lynas <adam@limitbreak.tech>
2019-05-20 21:59:31 +0100

    Added Meal, Ingredient and Base models in a new Core project. Added MealEntryPageViewModel for adding meals. Added MealStrings Resx for localisation.

---
+++ HappyBody.Core/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HappyBody.Core.Models
+{
+    public class Ingredient : BaseModel
+    {
+        public Ingredient()
+        {
+        }
+
+        public Guid MealId { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ HappyBody/ViewModels/MealEntryPageViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using HappyBody.Core.Models;
+using HappyBody.Localisation;
+using HappyBody.ViewModels;
+using Xamarin.Forms;
+
+namespace HappyBodyApp.ViewModels
+{
+    public class MealEntryPageViewModel : BaseViewModel
+    {
+        public Meal Meal { get; set; }
+        public bool ReviewNow { get; set; }
+
+        public ICommand SaveMealCommand;
+        public ICommand DeleteMealCommand;
+
+        public MealEntryPageViewModel()
+        {
+            Title = MealStrings.MealEntryTitle;
+
+            Meal = new Meal { Description = MealStrings.DefaultMealText };
+
+            SaveMealCommand = new Command(async () => await ExecuteSaveCommand());
+            DeleteMealCommand = new Command(async () => await ExecuteDeleteCommand());
+        }
+
+        async Task ExecuteSaveCommand()
+        {
+            if (IsBusy)
+                return;
+            IsBusy = true;
+
+            try
+            {
+                //TODO: Save Meal
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[{nameof(MealEntryPageViewModel)}] Save error: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        async Task ExecuteDeleteCommand()
+        {
+            if (IsBusy)
+                return;
+            IsBusy = true;
+
+            try
+            {
+                //TODO: 
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[{nameof(MealEntryPageViewModel)}] Delete error: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+}
+}
