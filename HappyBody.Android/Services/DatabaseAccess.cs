13e41bbbfba56bff4e368fe254340bcf3d115cda
Adam Bandeira Lynas <adam@limitbreak.tech>
2019-06-09 18:07:36 +0100

    Added LiteDb datastore

---
+++ HappyBody.Android/Services/DatabaseAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using HappyBody.Droid.Services;
+using HappyBody.Models;
+using HappyBody.Services;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DatabaseAccess))]
+namespace HappyBody.Droid.Services
+{
+    public class DatabaseAccess : IDatabaseAccess
+    {
+        public string DatabasePath()
+        {
+            var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), Constants.LocalDbName);
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+            }
+
+            return path;
+        }
+    }
+}
---
+++ HappyBody/Services/IDatabaseAccess.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace HappyBody.Services
+{
+    public interface IDatabaseAccess
+    {
+        string DatabasePath();
+    }
+}
